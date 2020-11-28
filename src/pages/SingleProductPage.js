78cc5e305d2a1bd0e59eb256febfcfa268509980
john-smilga <johnsmilga@gmail.com>
2020-11-28 08:54:43 -0800

    stars - manual approach

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -60,11 +60,11 @@ const SingleProductPage = () => {
         <Link to='/products' className='btn'>
           back to products
         </Link>
-        <div className='product-center'>
+        <div className=' product-center'>
           <ProductImages images={images} />
           <section className='content'>
             <h2>{name}</h2>
-            <Stars />
+            <Stars stars={stars} reviews={reviews} />
             <h5 className='price'> {formatPrice(price)}</h5>
             <p className='desc'> {description}</p>
             <p className='info'>
