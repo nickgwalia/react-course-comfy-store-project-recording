f91972cae91f27f5b980804dcd2321344f1363c4
john-smilga <johnsmilga@gmail.com>
2020-12-02 12:57:31 -0800

    filters : getUniqueValues

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -64,7 +64,7 @@ const filter_reducer = (state, action) => {
     return { ...state, filters: { ...state.filters, [name]: value } }
   }
   if (action.type === FILTER_PRODUCTS) {
-    console.log('filtering products')
+    // console.log('filtering products')
     return { ...state }
   }
   throw new Error(`No Matching "${action.type}" - action type`)
